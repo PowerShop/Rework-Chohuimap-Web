6113464bca326e66491ed7e4d5ac57c1784c48a0
PowerShop <aaaxcvg@gmail.com>
2024-10-06 22:43:32 +0700

    modified:   _dist/_css/_main.css 	new file:   _dist/_img/circle_blue.png 	new file:   _dist/_img/circle_gray_mini.png 	new file:   _dist/_img/circle_orange.png 	new file:   _dist/_img/clock.png 	new file:   _dist/_img/drink.png 	new file:   _dist/_img/end.png 	new file:   _dist/_img/food.png 	new file:   _dist/_img/icon.png 	new file:   _dist/_img/medicine.png 	new file:   _dist/_img/pin.png 	new file:   _dist/_img/snack.png 	new file:   _dist/_img/start.png 	new file:   _dist/_img/user_location.png 	modified:   _pages/home.php 	modified:   _pages/navbar.php

--- _pages/home.php
+++ _pages/home.php
@@ -1,5 +1,61 @@
-<div
-    class="alert alert-primary"
-    role="alert">
-    <i class="fa-brands fa-android"></i>
+<div class="container">
+    <div class="card text-white" style="background-color: rgb(95, 124, 217);">
+        <div class="card-body">
+            <h3 class="mt-2 text-center"><i class="fa-solid fa-bag-shopping"></i> ซื้ออะไรดี?</h3>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="ค้นหาที่นี่" id="search_location">
+                <!-- <button class="btn btn-primary" onclick="search();"><i class="fa fa-search" aria-hidden="true"></i> ค้นหา</button> -->
+            </div>
+        </div>
+    </div>
+
+    <div id="no-data-found"></div>
+    <!-- 3 Cards -->
+    <div class="row mt-3" id="quick_search">
+
+        <h3 class="text-start"><i class="fa-solid fa-magnifying-glass" style="--fa-bounce-land-scale-x: 1.05;--fa-bounce-land-scale-y: .8;--fa-bounce-rebound: 5px;"></i> ค้นหาด่วน</h3>
+        <div class="col-md-2 mb-2">
+            <div class="card border border-0" style="background-color: rgb(63, 155, 241);">
+                <div class="card-body text-center">
+                    <a href="?quick=food" class="text-decoration-none text-white">
+                        <img src="_dist/_img/food.png" class="img-fluid" alt="food" />
+                    </a>
+                </div>
+            </div>
+            <h5 class="mt-2"><i class="fa-solid fa-bowl-food"></i> อาหาร</h5>
+        </div>
+
+        <div class="col-md-2 mb-2">
+            <div class="card border border-0" style="background-color: rgb(246, 162, 0);">
+                <div class="card-body text-center">
+                    <a href="?quick=drink" class="text-decoration-none text-white">
+                        <img src="_dist/_img/drink.png" class="img-fluid" alt="food" />
+                    </a>
+                </div>
+            </div>
+            <h5 class="mt-2"><i class="fa-solid fa-wine-bottle"></i> เครื่องดื่ม</h5>
+        </div>
+
+        <div class="col-md-2 mb-2">
+            <div class="card border border-0" style="background-color: rgb(237, 39, 39);">
+                <div class="card-body text-center">
+                    <a href="?quick=snack" class="text-decoration-none text-white">
+                        <img src="_dist/_img/snack.png" class="img-fluid" alt="food" />
+                    </a>
+                </div>
+            </div>
+            <h5 class="mt-2"><i class="fa-solid fa-cookie-bite"></i> ของทานเล่น</h5>
+        </div>
+
+        <div class="col-md-2 mb-2">
+            <div class="card border bordeer-0" style="background-color: rgba(0, 128, 0, 0.5);">
+                <div class="card-body text-center">
+                    <a href="?quick=medicine" class="text-decoration-none text-white">
+                        <img src="_dist/_img/medicine.png" class="img-fluid" alt="medicine" width="64px" height="64px" />
+                    </a>
+                </div>
+            </div>
+            <h5 class="mt-2"><i class="fa-solid fa-pills"></i> ยารักษาโรค</h5>
+        </div>
+    </div>
 </div>
---
+++ _pages/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md bg-body py-3">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+
+            <span><img src="_dist/_favicon/favicon-32x32.png" alt="โชห่วยแมพ"> โชห่วยแมพ</span>
+        </a>
+        <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-4">
+            <span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-grow-0 order-md-first" id="navcol-4">
+            <ul class="navbar-nav me-auto">
+                <!-- <li class="nav-item"><a class="nav-link active" href="quick-find.php">ค้นหาด่วน</a></li> -->
+                <li class="nav-item"><a class="nav-link active" href="index.php"><i class="fa-solid fa-home"></i> หน้าหลัก</a></li>
+            </ul>
+
+        </div>
+        <!-- <div class="d-none d-md-block"><button class="btn btn-light me-2" type="button">Button</button>
+            <a class="btn btn-primary" role="button" href="#">Button</a>
+        </div> -->
+    </div>
+</nav>
