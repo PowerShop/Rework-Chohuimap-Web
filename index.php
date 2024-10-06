fbb28a59430aa8965da9bb9074cd2aa1939da413
PowerShop <aaaxcvg@gmail.com>
2024-10-06 22:42:41 +0700

    Changes to be committed: 	modified:   index.php
    Changes not staged for commit:
    	modified:   _dist/_css/_main.css
    	modified:   _pages/home.php
    	modified:   _pages/navbar.php
    Untracked files:
    	_dist/_img/

--- index.php
+++ index.php
@@ -27,6 +27,9 @@
     <link rel="icon" type="image/png" sizes="32x32" href="_dist/_favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="_dist/_favicon/favicon-16x16.png">
     <link rel="manifest" href="_dist/_favicon/site.webmanifest">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="_dist/_css/_main.css" />
 </head>
 
 <body>
