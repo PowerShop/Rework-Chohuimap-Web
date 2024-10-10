090c4bf378f50aad5fb499520447b13729a972c4
.xyz <aaaxcvg@gmail.com>
2024-10-10 17:59:48 +0700

    Changes to be committed: 	modified:   _pages/map.php

--- _pages/map.php
+++ _pages/map.php
@@ -127,7 +127,6 @@
                         },
                         error => {
                             reject(error);
-
                         }
                     );
                 } else {
